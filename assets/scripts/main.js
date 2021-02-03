867764b7609a897d927ddf729a3ae8216fb84551
Fernando Bracamonte <56770413+Fernandobrac@users.noreply.github.com>
2021-02-02 19:49:37 -0800

    Called volume image function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,25 +24,30 @@ const soundLevel = {
 
 
 //function to update the image correcponding to volume
-hornSound.volume = (volumeNumber.value)/100.0;  //volume is range from 0 to 1
-if(volumeNumber.value == level0){
-    volumeImage.scr = "./assets/media/icons/volume-level-0.svg";
-    horn_btn.disabled = true;   //dissable button if it is 0
-}
-else if(volumeNumber.value <= level1){
-    volumeImage.scr = "./assets/media/icons/volume-level-1.svg";
-}
-else if(volumeNumber.value <= level2){
-    volumeImage.scr = "./assets/media/icons/volume-level-2.svg";
-}
-else if(volumeNumber.value <= level3){
-    volumeImage.scr = "./assets/media/icons/volume-level-3.svg";
-}
-else{
-    volumeImage.scr = "./assets/media/icons/volume-level-0.svg";
-    horn_btn.disabled = true;   //dissable button if not in range of 0-100
+function changeVolumeIcon(){
+    hornSound.volume = (volumeNumber.value)/100.0;  //volume is range from 0 to 1
+    if(volumeNumber.value == level0){
+        volumeImage.scr = "./assets/media/icons/volume-level-0.svg";
+        horn_btn.disabled = true;   //dissable button if it is 0
+    }
+    else if(volumeNumber.value <= level1){
+        volumeImage.scr = "./assets/media/icons/volume-level-1.svg";
+    }
+    else if(volumeNumber.value <= level2){
+        volumeImage.scr = "./assets/media/icons/volume-level-2.svg";
+    }
+    else if(volumeNumber.value <= level3){
+        volumeImage.scr = "./assets/media/icons/volume-level-3.svg";
+    }
+    else{
+        volumeImage.scr = "./assets/media/icons/volume-level-0.svg";
+        horn_btn.disabled = true;   //dissable button if not in range of 0-100
+    }
 }
 
+//call changeVolumeIcon function
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+volumeNumber.addEventListener("input", updateVolume);
 
 //function to update the 3 radios (car/air/party)
 
