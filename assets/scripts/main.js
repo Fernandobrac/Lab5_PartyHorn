f6fc68b9d54fa9b91b6270434a54eccfe85268a4
Fernando Bracamonte <56770413+Fernandobrac@users.noreply.github.com>
2021-02-02 17:22:13 -0800

    added variables

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,14 @@
 // main.js
 
 // TODO
+let volumeNumber = document.getElementById("volume-number");
+let volumeImage = document.getElementById("volume-image");
+let volumeSlider = document.getElementById("volume-slider");
+
+let airHorn = document.getElementById("radio-air-horn");
+let carHorn = document.getElementById("radio-car-horn");
+let partyHorn = document.getElementById("radio-party-horn");
+let hornSound = document.getElementById("horn-sound");
+let honkButton = document.getElementById("honk-btn");
+
+let soundImage = document.getElementById("sound-image");
